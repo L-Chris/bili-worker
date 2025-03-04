13bbb3e8217b4f3738a1f92eef43ec0a746050c1
L-Chris <563303226@qq.com>
2025-03-04 14:01:36 +0800

    fix: 简化字幕

--- utils.ts
+++ utils.ts
@@ -71,6 +71,5 @@ export const formatTimestamp = (timestamp: number) => {
   const hours = Math.floor(time / 3600).toString().padStart(2, '0');
   const minutes = Math.floor(time / 60 % 60).toString().padStart(2, '0');
   const seconds = Math.floor(time % 60).toString().padStart(2, '0');
-  const ms = Math.floor((time - Math.floor(time)) * 100).toString().padStart(2, '0');
-  return `${hours}:${minutes}:${seconds},${ms}`;
+  return `${hours}:${minutes}:${seconds}`;
 };
